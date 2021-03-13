c3310b871508054dc0b8b97b37386e2e6c7c06fe
IB Teguh <teguh.teja@gmail.com>
2021-03-14 00:28:58 +0800

    04 Django Blog Menampilkan Model ke Home Page

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('blog/', views.blog, name='blog'),
 ]
 
-urlpatterns += static(settings.STATIC_URL, documents_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_blog/myblog/views.py
+++ django_blog/myblog/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import *
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    queryset = Post.objects.all()
+    context = {
+        'queryset': queryset
+    }
+    return render(request, 'index.html', context)
 
 
 def blog(request):
+    # blog = get_object_or_404(Post, id=blog_id)
     return render(request, 'blog.html')
