a4c65794ab72eff35db45ee5c264fa216126720c
IB Teguh <teguh.teja@gmail.com>
2021-03-14 09:27:37 +0800

    06 Django Blog Paginasi

--- django_blog/myblog/views.py
+++ django_blog/myblog/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 # Create your views here.
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def index(request):
     queryset = Post.objects.all()
+    # 2 means 1 page contain 2 post
+    paginator = Paginator(queryset, 2)
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        paginated_queryset = paginator.page(page)
+    except PageNotAnInteger:
+        paginated_queryset = paginator.page(1)
+    except EmptyPage:
+        paginated_queryset = paginator.page(paginator.num_pages)
+
     context = {
-        'queryset': queryset
+        'queryset': paginated_queryset,
+        'page_request_var': page_request_var
     }
     return render(request, 'index.html', context)
 
